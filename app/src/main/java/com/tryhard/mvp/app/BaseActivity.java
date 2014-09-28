2d6661318c594ffe7c8be97537ca935c2f4e1cbf
juancjara <juanc.jara@pucp.pe>
2014-09-27 20:04:17 -0500

    back button

--- app/src/main/java/com/tryhard/mvp/app/BaseActivity.java
+++ app/src/main/java/com/tryhard/mvp/app/BaseActivity.java
@@ -1,151 +1,45 @@
 package com.tryhard.mvp.app;
 
-import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 
-/**
- * Created by juancarlos on 23/09/14.
- */
+
 public class BaseActivity extends ActionBarActivity {
-    private CharSequence mTitle;
-    private ListView navList;
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-    private ActionBar actionBar;
-    private RelativeLayout navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.base_layout);
 
-        mTitle = getTitle();
-        actionBar = getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
 
         actionBar.setBackgroundDrawable(
-            new ColorDrawable(getResources().getColor(R.color.bg_action_bar)));
-        this.drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        this.navDrawer = (RelativeLayout)findViewById(R.id.nav_drawer);
-        this.navList = (ListView) findViewById(R.id.nav_list);
+                new ColorDrawable(getResources().getColor(R.color.bg_action_bar)));
 
-        // Load an array of options names
-        final String[] names = getResources().getStringArray(
-                R.array.nav_options);
-
-        // Set previous array as adapter of the list
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.side_menu_item, names);
-        navList.setAdapter(adapter);
-        navList.setOnItemClickListener(new DrawerItemClickListener());
-
-        //icono toggle
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
-                R.drawable.ic_drawer_white, R.string.open_drawer,
-                R.string.close_drawer) {
-
-            /**
-             * Called when a drawer has settled in a completely closed state.
-             */
-            public void onDrawerClosed(View view) {
-                getSupportActionBar().setTitle(mTitle);
-                // creates call to onPrepareOptionsMenu()
-                supportInvalidateOptionsMenu();
-            }
-            /**
-             * Called when a drawer has settled in a completely open state.
-             */
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle("Selecciona opción");
-                // creates call to onPrepareOptionsMenu()
-                supportInvalidateOptionsMenu();
-            }
-        };
-
-        // Set the drawer toggle as the DrawerListener
-        drawerLayout.setDrawerListener(drawerToggle);
-
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
+        setContentView(R.layout.base_layout);
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        // Called by the system when the device configuration changes while your
-        // activity is running
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle your other action bar items...
-        return super.onOptionsItemSelected(item);
-    }
-
-    /*
-     * Called whenever we call invalidateOptionsMenu()
-     */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content
-        // view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(navDrawer);
-        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    private class DrawerItemClickListener implements
-            ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position,
-                                long id) {
-            selectItem(position);
-        }
-    }
-
-    /** Swaps fragments in the main content view */
-    private void selectItem(int position) {
-        // Get text from resources
-        mTitle = getResources().getStringArray(R.array.nav_options)[position];
-        navList.setItemChecked(position, true);
-        actionBar.setTitle(mTitle);
-        drawerLayout.closeDrawer(navDrawer);
-        Intent intent = new Intent(this, BusStopActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/tryhard/mvp/app/BusStopActivity.java
+++ app/src/main/java/com/tryhard/mvp/app/BusStopActivity.java
@@ -2,10 +2,7 @@ package com.tryhard.mvp.app;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class BusStopActivity extends BaseActivity {
@@ -15,11 +12,10 @@ public class BusStopActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        data = new BusStopInformation[4];
+        data = new BusStopInformation[3];
         data[0] = new BusStopInformation("301", R.drawable.m_301);
         data[1] = new BusStopInformation("302", R.drawable.map2);
         data[2] = new BusStopInformation("303", R.drawable.map2);
-        data[3] = new BusStopInformation("304", R.drawable.map2);
 
         findViewById(R.id.content_frame);
         Fragment fragment = new BusStopFragment();
