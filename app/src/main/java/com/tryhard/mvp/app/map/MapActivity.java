9ff44681826ad73e2d9f0e85a1163f2fc73ef970
andreqi <drol.005@gmail.com>
2014-09-29 22:13:06 -0500

    erase handler test

--- app/src/main/java/com/tryhard/mvp/app/map/MapActivity.java
+++ app/src/main/java/com/tryhard/mvp/app/map/MapActivity.java
@@ -20,31 +20,5 @@ public class MapActivity extends BaseActivity {
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit();
-        HandlerThread thread = new HandlerThread("churrehandler");
-        thread.start();
-        Looper looper = thread.getLooper();
-        Handler handler = new Handler(looper) {
-            @Override
-            public void handleMessage(Message msg) {
-                try {
-                    switch (msg.what) {
-                        case 0:
-                            Thread.sleep(10000);
-                            System.out.println("happy 10 segs");
-                            break;
-                        case 1:
-                            Thread.sleep(5000);
-                            System.out.println("happy 5 segs");
-                            break;
-                    }
-                } catch (Exception e) {
-                    // should not happen
-                }
-            }
-        };
-        handler.sendMessage(handler.obtainMessage(0));
-        System.out.println("gg wp 0");
-        handler.sendMessage(handler.obtainMessage(1));
-        System.out.println("gg wp 1");
     }
 }
